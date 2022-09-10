f31b7a24e584f0f8a2982622c504e394d35124f7
Ajiwunmi <ajiwunmi@gmail.com>
2022-09-09 17:22:33 -0700

    Alphabet a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: 'Checks a random number if positive, negative or Zero'
+ *
+ * Return: return 0 to indicate end of program
+ */
+int main(void)
+{
+
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+
+}
