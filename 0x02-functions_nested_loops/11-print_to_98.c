af205d948efe5e4a99dd678d354dfccece8c7f16
Ajiwunmi <taiwo.it@gmail.com>
2022-09-14 05:29:10 +0100

    loop updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,18 +12,32 @@
  */
 void print_to_98(int n)
 {
+
+    int i;
     if (n <= 98)
     {
-        int i;
-
+     
         for(i = n; i <= 98; i++)
         {
+
             if(i != 98)
                 printf("%d, ",i);
             else    
                 printf("%d",i);
         }
     }
+    else
+    {
+        for(i = n; i = 98; i--)
+        {
+
+            if(i != 98)
+                printf("%d, ",i);
+            else    
+                printf("%d",i);
+        }
+    }
+
     printf("\n");
 }
 
