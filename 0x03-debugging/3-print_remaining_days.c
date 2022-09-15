6322d88bb96754fc385939470531c1f724066204
Ajiwunmi <ajiwunmi@gmail.com>
2022-09-15 12:56:48 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,24 +15,19 @@
  *
  */
 
-
-
 void print_remaining_days(int month, int day, int year)
-	
 {
 	
 	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 !=  0))
 	{
 		
-		if (month >= 3 && day >= 60)
-			
+		if (month >= 3 && day >= 60)	
 		{
 			
 			day++;
 			
 		}
 		
-			
 		printf("Day of the year: %d\n", day);
 		
 		printf("Remaining days: %d\n", 366 - day);
@@ -55,7 +50,6 @@ void print_remaining_days(int month, int day, int year)
 			printf("Remaining days: %d\n", 365 - day);
 			
 		}
-		
 	}
 	
 }
