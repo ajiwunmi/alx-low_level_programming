8b6c6d4d664e0ccdde3b641cc0f000b3dc623da4
Ajiwunmi <taiwo.it@gmail.com>
2022-11-04 13:37:31 +0100

    0x15-read-file updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,19 +13,20 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	
 	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	buffer - malloc(sizeof(char) * letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO,buffers, r);
+	w = write(STDOUT_FILENO,buffer, r);
 
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
@@ -36,4 +37,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buffer);
 	close(o);
 
-	return (w)
+	return (w);
