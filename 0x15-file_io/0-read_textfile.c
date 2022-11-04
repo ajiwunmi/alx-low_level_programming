68e61ab34d7324071f2a31e48c73217c228fd776
Ajiwunmi <taiwo.it@gmail.com>
2022-11-04 13:04:41 +0100

    0x15-file_io updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,7 @@
  * @filename: A pointer to the name of the file.
  * @letters: The number of letters the function should read and print
  *
- * Return : if the function fails or filename is NULL - #
+ * Return : if the function fails or filename is NULL - 0
  * 	O/w -  the actual number of bytes the function can read and print
  * 	
  */
@@ -17,11 +17,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 
 	if (filename == NULL)
-		return (#);
+		return (0);
 
 	buffer - malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-		return (#);
+		return (0);
 
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
@@ -30,7 +30,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
-		return (#);
+		return (0);
 	}
 
 	free(buffer);
