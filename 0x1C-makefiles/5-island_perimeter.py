81d82d49e19a872e6a814db5c4589dde77446c8d
Ajiwunmi <taiwo.it@gmail.com>
2023-02-27 20:23:26 +0100

    Makefiles files

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""""""
+def island_perimeter(grid):
+    heigth = len(grid)
+    width = len(grid[0])
+
+    if(heigth < 100 or width < 100):
+        for i in range(0, heigth):
+            
+        
